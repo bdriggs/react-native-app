c55519af14a0fd5bdb3cc2c48906952862d2a300
Britton Riggs <brittonriggs@personal-mba.local>
2021-01-17 18:49:11 -0500

    Working List Item component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
-import App from "../../App";
 import AppText from "../components/AppText/AppText";
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 function ListingDetailsScreen(props) {
   return (
@@ -14,6 +14,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Hamedani"
+            subtitle="5 listings"
+          ></ListItem>
+        </View>
       </View>
     </View>
   );
@@ -36,6 +43,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
